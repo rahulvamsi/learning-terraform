9dfe86f920bfd9b3ad0a882bf35c4f3ca5e05526
Raghu K <raghuk.vit@gmail.com>
2022-09-06 08:16:16 +0530

    Update learning-terraform/

--- 05-loops/for-each/main.tf
+++ 05-loops/for-each/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  for_each      = var.sample
+  for_each      = toset(var.sample)
   ami           = "ami-07ef4d849950da0b3"
   instance_type = "t3.micro"
   tags = {
