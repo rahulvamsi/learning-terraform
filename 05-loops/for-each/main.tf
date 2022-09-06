b739361a01adff81a4caa9d70d013f78f6503934
Raghu K <raghuk.vit@gmail.com>
2022-09-06 08:15:15 +0530

    Update learning-terraform/

---
+++ 05-loops/for-each/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "web" {
+  for_each      = var.sample
+  ami           = "ami-07ef4d849950da0b3"
+  instance_type = "t3.micro"
+  tags = {
+    Name = each.key
+  }
+}
+
+variable "sample" {
+  default = ["one", "two"]
+}
+
