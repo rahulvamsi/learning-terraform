0847d3c505bc62bfd8c1f57daf23b384850a277b
Raghu K <raghuk.vit@gmail.com>
2022-09-07 07:48:58 +0530

    Update learning-terraform/

---
+++ 07-provisioners/main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "web" {
+  ami                    = "ami-07ef4d849950da0b3"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-02afc752c16b2fa2e"]
+
+  provisioner "remote-exec" {
+
+    connection {
+      host     = self.public_ip
+      username = "root"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "echo Hello World"
+    ]
+
+  }
+}
