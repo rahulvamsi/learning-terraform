b39b368e31b9d3fd12d7071968a754af57ec4398
rahulvamsi <p.rahulvamsi@gmail.com>
2022-09-13 16:48:36 +0530

    updated terraform code

--- 03-remote-state/sample.tf
+++ 03-remote-state/sample.tf
@@ -1,12 +1,13 @@
 terraform {
   backend "s3" {
     bucket = "terraform-rb66"
-    key    = "sample/terraform.tfstate"
+    key    = "sample1/terraform.tfstate"
     region = "us-east-1"
   }
 }
 
 resource "aws_instance" "web" {
+  
   ami           = "ami-05403c07e1c840e24"
   instance_type = "t3.micro"
 }
